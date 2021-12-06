7a9ef31939d48605bac30986e8e136bce62deef0
jackp <jackp@PC>
2021-12-06 10:04:49 +0200

    Add project files.

---
+++ BT_AUTO_2021_Programing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT_AUTO_2021_Programing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
