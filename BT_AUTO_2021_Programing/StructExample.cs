3dd6c52d714d3ca63e70cbcada8664f81c280924
ioaniakab <95611262+ioaniakab@users.noreply.github.com>
2021-12-19 14:10:23 +0200

    Add files via upload

---
+++ BT_AUTO_2021_Programing/StructExample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT_AUTO_2021_Programing
+{
+    public class StructExample
+    {
+        public struct MyStruct
+        {
+            int number;
+            string testString;
+
+            public void Assign(int number, string testString)
+            {
+                this.number = number;
+                this.testString = testString;
+            }
+
+            public int ComputeSum()
+            {
+                int sum = 0;
+                for (int i=0; i<number; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+
+        public struct Rectangle3d
+        {
+            double l;
+            double L;
+            double h;
+
+            public void Assign(double l, double L, double  h)
+            {
+                this.l = l;
+                this.L = L;
+                this.h = h;
+            }
+
+            public int GetVertices()
+            {
+                return 12;
+            }
+
+            public double GetArea()
+            {
+                return 4 * L * h + 2 * L * L;
+            }
+
+            public double GetVolume()
+            {
+                return l * L * h;
+            }
+        }
+
+
+    }
+}
