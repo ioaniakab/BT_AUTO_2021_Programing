3781ca132d3b2b97a553554a24761ba0d668279a
ioaniakab <95611262+ioaniakab@users.noreply.github.com>
2021-12-19 14:03:25 +0200

    Update Circle.cs

--- BT_AUTO_2021_Programing/Circle.cs
+++ BT_AUTO_2021_Programing/Circle.cs
@@ -1,25 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
-/*
+
 namespace BT_AUTO_2021_Programing
 {
     class Circle
     {
-        double Radius;
-    }
+        double radius;
+        // const double PI = 3.1415;
 
-    public void SetRadius(double circleRadius)
-    {
-       this.Radius = circleRadius;
-    }
-    public double GetArea()
-    {
-        return Math.PI * Math.Pow(radius, 2);
-    }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
 
-    public void PrintCircle()
-    {
-        Console.WriteLine("Circle with radius {0} has the area {1}", radius, GetArea());
+        public Circle()
+        {
+            this.radius = 0;
+        }
+        public void SetRadius(double circleRadius)
+        {
+            radius = circleRadius;
+        }
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public void PrintCircle()
+        {
+            Console.WriteLine("Circle with radius {0} has the area {1}", radius, GetArea());
+
+        }
+
     }
-} */
+}
